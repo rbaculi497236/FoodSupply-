e9e12291ba40c931185ffebc6fcce9e1ae319cc5
Rey Jhoniel Baculi <r.baculi.497236@umindanao.edu.ph>
2026-09-21 00:25:49 +0800

    update

--- obj/Debug/net9.0/FoodSupply.AssemblyInfo.cs
+++ obj/Debug/net9.0/FoodSupply.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodSupply")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73d7dea151404589c429779244db541fca0f48d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3e465211a338559266e2c0fe7cb2c0ac41de784")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodSupply")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodSupply")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
